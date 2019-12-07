e77e23dd3841bc89b94ee3427615b3d5f735bd2f
Joern Petersen <petersenjoern@protonmail.com>
2019-12-07 15:51:10 +0100

    added Fill.asm (keyboard)

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,31 +12,69 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
-    @status
-    M=-1        // status=0xFFFF
-    D=0         // Argument - what to set screen bits to
-    @SETSCREEN
-    0;JMP
+@SCREEN
+D=A
+@Position //initial position to -1, if to 0 there will be a bug when whiten the screen
+M=D-1
 
-(LOOP)
-    @KBD
-    D=M         // D = current keyboard character
-    @SETSCREEN
-    D;JEQ       // If no key, set screen to zeroes (white)
-    D=-1        // If key pressed, set screen to all 1 bits (black)
-    
-(SETSCREEN)     // Set D=new status before jumping here
-    @ARG
-    M=D         // Save new status arg
-    @status     // FFFF=black, 0=white - status of entire screen
-    D=D-M       // D=newstatus-status
-    @LOOP
-    D;JEQ        // Do nothing if new status == old status
-    
-    @ARG
-    D=M
-    @status
-    M=D         // status = ARG
-    
-    @SCREEN
-    D=A        
+(CHECK)
+@KBD
+D=M      //Read keyboard input
+@BLACKEN //if the key value is greater than 0, go to blacken
+D;JGT
+@WHITEN  //else go to whiten
+0;JMP
+
+//Start blacken the Screen
+(BLACKEN)
+//if position is at max of the screen, do nothing
+@24576
+D=M
+@Position
+D=D-M
+@CHECK
+D;JEQ
+
+//else start blacken
+@Position
+A=M
+M=-1
+
+//go to next position
+@Position
+D=M+1
+@Position
+M=D
+
+//go to check if user is still hold the key
+@CHECK
+0;JMP
+
+
+//Start whiten the Screen
+(WHITEN)
+
+//if position is at left top of the screen, do nothing
+@SCREEN
+D=A-1    
+//if it is D=A there will be a line on the left top of the screen 
+//that can not be removed
+@Position
+D=D-M
+@CHECK
+D;JEQ
+
+//else start whiten
+@Position
+A=M
+M=0
+
+//draw back position
+@Position
+D=M-1
+@Position
+M=D
+
+//check if there is any key pressed by user
+@CHECK
+0;JMP
